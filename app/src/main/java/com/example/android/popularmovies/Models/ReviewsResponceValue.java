f4759d7c05f8933e6d91eaa6b152ed8881ab64f9
haya <haya.akkad94@gmail.com>
2018-05-10 19:56:29 +0300

    stage 2

---
+++ app/src/main/java/com/example/android/popularmovies/Models/ReviewsResponceValue.java
@@ -0,0 +1,113 @@
+package com.example.android.popularmovies.Models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+
+
+public class ReviewsResponceValue  extends RealmObject implements Parcelable
+{
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("results")
+    @Expose
+    private RealmList<Reviews> results = null;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    public final static Parcelable.Creator<ReviewsResponceValue> CREATOR = new Creator<ReviewsResponceValue>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public ReviewsResponceValue createFromParcel(Parcel in) {
+            return new ReviewsResponceValue(in);
+        }
+
+        public ReviewsResponceValue[] newArray(int size) {
+            return (new ReviewsResponceValue[size]);
+        }
+
+    }
+            ;
+
+    protected ReviewsResponceValue(Parcel in) {
+        this.id = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.page = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        in.readList(this.results, (com.example.android.popularmovies.Models.Reviews.class.getClassLoader()));
+        this.totalPages = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.totalResults = ((Integer) in.readValue((Integer.class.getClassLoader())));
+    }
+
+    public ReviewsResponceValue() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public RealmList<Reviews> getResults() {
+        return results;
+    }
+
+    public void setResults( RealmList<Reviews> results) {
+        this.results = results;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(id);
+        dest.writeValue(page);
+        dest.writeList(results);
+        dest.writeValue(totalPages);
+        dest.writeValue(totalResults);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}
